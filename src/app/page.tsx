20d8b473e519c6930f8bf6db5c0f93510defbecb
MChamuka <maddewithanagechamuka@gmail.com>
2025-03-26 17:32:49 +0530

    below bbar teext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,43 @@ import { useState, useEffect } from "react";
 
 export default function Page() {
   const [progress, setProgress] = useState(0);
+  const [loadText,setLoadText] = useState("Starting...")
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress((prev) => (prev < 100 ? prev + 10 : 100));
+      setProgress((prev) => {
+        const newProgress = prev < 100 ? prev + 10 : 100;
+        
+        if (newProgress <= 25) {
+          setLoadText("Starting...");
+        } else if (newProgress <= 50) {
+          setLoadText("Please wait...");
+        } else if (newProgress <= 75) {
+          setLoadText("Almost finished...");
+        } else {
+          setLoadText("Completed");
+        }
+
+        return newProgress;
+      });
     }, 500);
 
     return () => clearInterval(interval);
   }, []);
-
   return (
     <div className="flex flex-col items-center justify-center h-screen px-4">
       <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center">
         Welcome to My Portfolio
       </h1>
-      <p className="text-sm sm:text-base font-medium mt-3">Booting...</p>
-      <div className=" w-full sm:w-140 md:w-180 lg:w-250 p-[2px] xl:w-300 2xl:w-350 h-15 bg-gray-300 mt-4 rounded-md overflow-hidden">
-        <div
-          className="h-full bg-blue-500 transition-all"
-          style={{ width: `${progress}%` }}
-        ></div>
+      <div className=" w-full sm:w-140 md:w-180 lg:w-250 p-[2px] xl:w-300 2xl:w-350 ">
+        <p className="text-xl font-medium mt-3">BOOTING...</p>
+        <div className=" w-full h-15 bg-gray-300 p-[2px] rounded-md overflow-hidden">
+          <div
+            className="h-full bg-blue-500 transition-all"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
+        <p className="text-sm sm:text-base font-medium mt-3">{loadText}</p>
       </div>
     </div>
   );
