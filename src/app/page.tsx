880b09dad9ec7e54f8416edc7ba27a1b066e4f1d
MChamuka <maddewithanagechamuka@gmail.com>
2025-03-26 16:45:30 +0530

    progreess bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";  // 👈 Add this at the top
+"use client";  
 
 import { useState, useEffect } from "react";
 
@@ -14,9 +14,12 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Welcome to My Portfolio</h1>
-      <div className="w-200 h-2 bg-gray-300 mt-4 rounded-full overflow-hidden">
+    <div className="flex flex-col items-center justify-center h-screen px-4">
+      <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center">
+        Welcome to My Portfolio
+      </h1>
+      <p className="text-sm sm:text-base font-medium mt-3">Booting...</p>
+      <div className=" w-full sm:w-140 md:w-180 lg:w-250 p-[2px] xl:w-300 2xl:w-350 h-15 bg-gray-300 mt-4 rounded-md overflow-hidden">
         <div
           className="h-full bg-blue-500 transition-all"
           style={{ width: `${progress}%` }}
